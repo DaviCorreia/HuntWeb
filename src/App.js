01e155119a8b4359f3baa23089dc575051089e03
DaviCorreia <davicorreia21@gmail.com>
2020-05-07 22:45:20 -0300

    add componentes e add CSS

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+import './styles.css';
+
+import Header from './components/Header';
+
+const App = () =>(
+
+  <div className="app">
+    <Header/>
+  </div>
+ 
+   
+);
+
+export default App;
