32cf800425bc14e378ca4746ae81f1116ee445ec
DaviCorreia <davicorreia21@gmail.com>
2020-05-08 18:11:37 -0300

    Buscando produtos da API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React,{Component} from 'react';
 
 import './styles.css';
 
+import Main from './pages/main';
+
 import Header from './components/Header';
 
 const App = () =>(
 
   <div className="app">
     <Header/>
+    <Main/>
   </div>
  
    
